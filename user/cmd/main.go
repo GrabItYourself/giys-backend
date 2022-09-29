301c937cc09cdc3455f061c6d1bbc349dff89da1
Nut Pinyo <bomb.np@gmail.com>
2022-09-29 16:55:04 +0700

    feat: create user rest service

--- user/cmd/main.go
+++ user/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		panic(errors.Wrap(err, "error during app listen"))
+	}
 }
