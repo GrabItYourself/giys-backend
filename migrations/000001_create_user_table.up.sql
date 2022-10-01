e1e07e1e5f94cd27c679780f7d6264a274f383fd
Nut Pinyo <bomb.np@gmail.com>
2022-10-01 15:33:09 +0700

    edit migration

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -3,7 +3,7 @@ BEGIN;
 CREATE TYPE role AS ENUM ('admin', 'user');
 CREATE TABLE IF NOT EXISTS "users" (
     id TEXT PRIMARY KEY,
-    role role NOT NULL,
+    role role NOT NULL DEFAULT 'user',
     email TEXT NOT NULL
 );
 
