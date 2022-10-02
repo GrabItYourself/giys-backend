e9e351bc64ced7dbb1a01ad107ab3357b14366d3
nonya <nonya579@gmail.com>
2022-10-02 11:48:23 +0700

    add payment server

--- payment/pkg/server/server.go
+++ payment/pkg/server/server.go
@@ -12,15 +12,22 @@ type server struct {
 	pb.UnimplementedPaymentServer
 }
 
-func (server) Pay(context.Context, *pb.PayRequest) (*pb.PayResponse, error) {
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) Pay(context.Context, *pb.PayRequest) (*pb.PayResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Pay not implemented")
 }
-func (server) AuthorizeCard(context.Context, *pb.AuthorizeCardRequest) (*pb.AuthorizeCardResponse, error) {
+
+func (s *server) AuthorizeCard(context.Context, *pb.AuthorizeCardRequest) (*pb.AuthorizeCardResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AuthorizeCard not implemented")
 }
-func (server) RegisterRecipient(context.Context, *pb.RegisterRecipientRequest) (*pb.RegisterRecipientResponse, error) {
+
+func (s *server) RegisterRecipient(context.Context, *pb.RegisterRecipientRequest) (*pb.RegisterRecipientResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RegisterRecipient not implemented")
 }
-func (server) GetTransactionHistory(context.Context, *pb.GetTransactionHistoryRequest) (*pb.GetTransactionHistoryResponse, error) {
+
+func (s *server) GetTransactionHistory(context.Context, *pb.GetTransactionHistoryRequest) (*pb.GetTransactionHistoryResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetTransactionHistory not implemented")
 }
