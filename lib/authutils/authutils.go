7c0863e7dd0fb6dfc52836152d6e5c3ad30655fa
Nut Pinyo <bomb.np@gmail.com>
2022-10-02 20:54:15 +0700

    add extract user metadata from context

--- lib/authutils/authutils.go
+++ lib/authutils/authutils.go
@@ -4,6 +4,8 @@ import (
 	"github.com/GrabItYourself/giys-backend/lib/proto/libproto"
 	"github.com/gofiber/fiber/v2"
 	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
 )
 
 const userHeader = "Auth-User-Id"
@@ -20,4 +22,24 @@ func ExtractUserFromContext(ctx *fiber.Ctx) (string, libproto.UserRole, error) {
 		return "", libproto.UserRole_NULL, errors.Errorf("header %s is empty", roleHeader)
 	}
 	return userId, libproto.UserRole(libproto.UserRole_value[role]), nil
+}
+
+func ExtractUserFromGrpcContext(ctx context.Context) (string, libproto.UserRole, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", libproto.UserRole_NULL, errors.New("can't get metadata from context")
+	}
+	userId := md.Get(userHeader)
+	if len(userId) == 0 {
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s is empty", userHeader)
+	} else if len(userId) > 1 {
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s has more than one value", userHeader)
+	}
+	role := md.Get(roleHeader)
+	if len(role) == 0 {
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s is empty", roleHeader)
+	} else if len(role) > 1 {
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s has more than one value", roleHeader)
+	}
+	return userId[0], libproto.UserRole(libproto.UserRole_value[role[0]]), nil
 }
