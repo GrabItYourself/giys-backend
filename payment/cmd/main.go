8fbcb9feff733d3cd525d0f8aad1b9ecad25a10c
nonya <nonya579@gmail.com>
2022-10-02 11:48:23 +0700

    add payment server

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"net"
+
 	"github.com/GrabItYourself/giys-backend/lib/logger"
+	pb "github.com/GrabItYourself/giys-backend/lib/proto/payment"
 	"github.com/GrabItYourself/giys-backend/payment/config"
+	"github.com/GrabItYourself/giys-backend/payment/pkg/server"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	conf := config.InitConfig()
 	logger.InitLogger(&conf.Log)
+
+	s := grpc.NewServer()
+    lis, err := net.Listen("tcp", ":" + conf.Server.Port)
+    if err != nil {
+		logger.Fatal("Failed to listen: " + err.Error())
+    }
+
+	pb.RegisterPaymentServer(s, server.NewServer())
+
+	err = s.Serve(lis)
+    if err != nil {
+		logger.Fatal("Failed to serve: " + err.Error())
+    }
 }
