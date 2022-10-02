da76eaca698bfe08ff1e7ff79c30ac2c904c3f48
Nut Pinyo <bomb.np@gmail.com>
2022-10-02 17:36:54 +0700

    use proto in authutils

--- lib/authutils/authutils.go
+++ lib/authutils/authutils.go
@@ -1,7 +1,7 @@
 package authutils
 
 import (
-	"github.com/GrabItYourself/giys-backend/lib/postgres/models"
+	"github.com/GrabItYourself/giys-backend/lib/proto/libproto"
 	"github.com/gofiber/fiber/v2"
 	"github.com/pkg/errors"
 )
@@ -10,14 +10,14 @@ const userHeader = "Auth-User-Id"
 const roleHeader = "Auth-User-Role"
 
 // ExtractUserFromContext extracts userId and role from context
-func ExtractUserFromContext(ctx *fiber.Ctx) (string, models.RoleEnum, error) {
+func ExtractUserFromContext(ctx *fiber.Ctx) (string, libproto.UserRole, error) {
 	userId := ctx.Get(userHeader)
 	if userId == "" {
-		return "", "", errors.Errorf("header %s is empty", userHeader)
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s is empty", userHeader)
 	}
 	role := ctx.Get(roleHeader)
 	if role == "" {
-		return "", "", errors.Errorf("header %s is empty", roleHeader)
+		return "", libproto.UserRole_NULL, errors.Errorf("header %s is empty", roleHeader)
 	}
-	return userId, models.RoleEnum(role), nil
+	return userId, libproto.UserRole(libproto.UserRole_value[role]), nil
 }
