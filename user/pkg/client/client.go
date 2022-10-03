4732b457cbaee8fd929c38cc9c57a8e5cc792612
Nut Pinyo <bomb.np@gmail.com>
2022-10-03 19:56:08 +0700

    add user client

---
+++ user/pkg/client/client.go
@@ -0,0 +1,18 @@
+package client
+
+import (
+	"github.com/GrabItYourself/giys-backend/user/internal/libproto"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+)
+
+func NewClient(addr string, opts ...grpc.DialOption) (*libproto.UserServiceClient, error) {
+	conn, err := grpc.Dial(addr, opts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to connect to user service")
+	}
+	defer conn.Close()
+
+	client := libproto.NewUserServiceClient(conn)
+	return &client, nil
+}
