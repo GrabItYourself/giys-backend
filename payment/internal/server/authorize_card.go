306f135496632611c2ef2141e4c56749ff390942
nonya <nonya579@gmail.com>
2022-10-04 12:52:56 +0700

    add AuthorizeCard

--- payment/internal/server/authorize_card.go
+++ payment/internal/server/authorize_card.go
@@ -2,12 +2,69 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	"github.com/GrabItYourself/giys-backend/lib/authutils"
+	"github.com/GrabItYourself/giys-backend/lib/postgres/models"
 	"github.com/GrabItYourself/giys-backend/payment/internal/libproto"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"github.com/omise/omise-go"
+	"github.com/omise/omise-go/operations"
 )
 
-func (s *Server) AuthorizeCard(context.Context, *libproto.AuthorizeCardRequest) (*libproto.AuthorizeCardResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AuthorizeCard not implemented")
+func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRequest) (*libproto.AuthorizeCardResponse, error) {
+	userId, _, err := authutils.ExtractUserFromGrpcContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	user, err := s.repo.GetUserById(userId)
+	if err != nil {
+		return nil, err
+	}
+
+	token, createToken := &omise.Token{}, &operations.CreateToken{
+		Name:            in.Name,
+		Number:          in.CardNumber,
+		ExpirationMonth: time.Month(in.ExpirationMonth),
+		ExpirationYear:  int(in.ExpirationYear),
+	}
+
+	if err := s.omiseClient.Do(token, createToken); err != nil {
+		fmt.Printf(in.GetName() + in.CardNumber)
+		return nil, err
+	}
+
+	// check if user has omise customer id
+	if user.OmiseCustomerId == nil {
+		customer, createCustomer := &omise.Customer{}, &operations.CreateCustomer{
+			Email: user.Email,
+			Card:  token.ID,
+		}
+
+		if err := s.omiseClient.Do(customer, createCustomer); err != nil {
+			return nil, err
+		}
+
+		s.repo.UpdateOmiseCustomerId(userId, customer.ID)
+
+	} else {
+		customer, updateCustomer := &omise.Customer{}, &operations.UpdateCustomer{
+			CustomerID: *user.OmiseCustomerId,
+			Card:       token.ID,
+		}
+
+		if err := s.omiseClient.Do(customer, updateCustomer); err != nil {
+			return nil, err
+		}
+	}
+
+	err = s.repo.CreatePaymentMethod(&models.PaymentMethod{
+		UserId:      userId,
+		OmiseCardId: token.Card.ID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &libproto.AuthorizeCardResponse{}, nil
 }
