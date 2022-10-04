be52b9e969e80e40923e65566582556d830bd986
nonya <nonya579@gmail.com>
2022-10-04 11:38:11 +0700

    change migration type

--- migrations/000006_create_payment_method_table.up.sql
+++ migrations/000006_create_payment_method_table.up.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS "payment_method" (
-    id TEXT PRIMARY KEY,
-    user_id TEXT NOT NULL,
+    id INT PRIMARY KEY,
+    user_id UUID NOT NULL,
     omise_card_id TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
