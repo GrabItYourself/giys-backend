5cf1f7d9030982e7bd87209e03f778ed615e65e0
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-10-04 23:31:11 +0700

    move shop's libproto to pkg and rename to shopproto

--- shop/internal/server/bank_account.go
+++ shop/internal/server/bank_account.go
@@ -6,9 +6,9 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/GrabItYourself/giys-backend/shop/internal/libproto"
+	"github.com/GrabItYourself/giys-backend/shop/pkg/shopproto"
 )
 
-func (*Server) AddBankAccount(ctx context.Context, input *libproto.AddBankAccountRequest) (*libproto.AddBankAccountResponse, error) {
+func (*Server) AddBankAccount(ctx context.Context, input *shopproto.AddBankAccountRequest) (*shopproto.AddBankAccountResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddBankAccount not implemented")
 }
--- shop/pkg/client/client.go
+++ shop/pkg/client/client.go
@@ -2,11 +2,11 @@ package client
 
 import (
 	"github.com/GrabItYourself/giys-backend/lib/logger"
-	"github.com/GrabItYourself/giys-backend/shop/internal/libproto"
+	"github.com/GrabItYourself/giys-backend/shop/pkg/shopproto"
 	"google.golang.org/grpc"
 )
 
-func NewClient(addr string, opts ...grpc.DialOption) (*libproto.ShopServiceClient, error) {
+func NewClient(addr string, opts ...grpc.DialOption) (shopproto.ShopServiceClient, error) {
 	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		logger.Error("Failed to connect to Shop service: " + err.Error())
@@ -14,6 +14,6 @@ func NewClient(addr string, opts ...grpc.DialOption) (*libproto.ShopServiceClien
 	}
 	defer conn.Close()
 
-	client := libproto.NewShopServiceClient(conn)
-	return &client, nil
+	client := shopproto.NewShopServiceClient(conn)
+	return client, nil
 }
