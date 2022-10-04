5cdf5b70d95d36c1bdb246dad094f638840d84a7
nonya <nonya579@gmail.com>
2022-10-04 23:30:49 +0700

    fix error

--- payment/internal/server/authorize_card.go
+++ payment/internal/server/authorize_card.go
@@ -5,21 +5,25 @@ import (
 	"time"
 
 	"github.com/GrabItYourself/giys-backend/lib/authutils"
+	"github.com/GrabItYourself/giys-backend/lib/postgres"
 	"github.com/GrabItYourself/giys-backend/lib/postgres/models"
 	"github.com/GrabItYourself/giys-backend/payment/internal/libproto"
 	"github.com/omise/omise-go"
 	"github.com/omise/omise-go/operations"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRequest) (*libproto.AuthorizeCardResponse, error) {
 	userId, _, err := authutils.ExtractUserFromGrpcContext(ctx)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(codes.Unauthenticated, errors.Wrap(err, "can't extract user from context").Error())
 	}
 
 	user, err := s.repo.GetUserById(userId)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(postgres.InferCodeFromError(err), errors.Wrap(err, "can't get user").Error())
 	}
 
 	token, createToken := &omise.Token{}, &operations.CreateToken{
@@ -30,7 +34,7 @@ func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRe
 	}
 	err = s.omiseClient.Do(token, createToken)
 	if err != nil {
-		return nil, err
+		return nil, status.Error(InferCodeFromError(err), errors.Wrap(err, "can't create token").Error())
 	}
 
 	if user.OmiseCustomerId == nil {
@@ -40,12 +44,12 @@ func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRe
 		}
 		err = s.omiseClient.Do(customer, createCustomer)
 		if err != nil {
-			return nil, err
+			return nil, status.Error(InferCodeFromError(err), errors.Wrap(err, "can't create omise customer").Error())
 		}
 
 		err = s.repo.UpdateOmiseCustomerId(userId, customer.ID)
 		if err != nil {
-			return nil, err
+			return nil, status.Error(postgres.InferCodeFromError(err), errors.Wrap(err, "can't update omise customer id").Error())
 		}
 
 	} else {
@@ -55,7 +59,7 @@ func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRe
 		}
 		err = s.omiseClient.Do(customer, updateCustomer)
 		if err != nil {
-			return nil, err
+			return nil, status.Error(InferCodeFromError(err), errors.Wrap(err, "can't add card").Error())
 		}
 	}
 
@@ -64,7 +68,7 @@ func (s *Server) AuthorizeCard(ctx context.Context, in *libproto.AuthorizeCardRe
 		OmiseCardId: token.Card.ID,
 	})
 	if err != nil {
-		return nil, err
+		return nil, status.Error(postgres.InferCodeFromError(err), errors.Wrap(err, "can't create payment method").Error())
 	}
 
 	return &libproto.AuthorizeCardResponse{}, nil
---
+++ payment/internal/server/error.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"github.com/omise/omise-go"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+)
+
+func InferCodeFromError(err error) codes.Code {
+	omiseError := &omise.Error{}
+	switch errors.As(err, &omiseError); omiseError.StatusCode {
+	case 400:
+		return codes.InvalidArgument
+	case 403:
+		return codes.FailedPrecondition
+	case 404:
+		return codes.NotFound
+	default:
+		return codes.Unavailable
+	}
+}
