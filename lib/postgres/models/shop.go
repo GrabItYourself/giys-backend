486bb64dc2da6bf0633449f825c01a5088ed32f2
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-10-05 19:05:03 +0700

    Rename ID to Id in gorm model definition

--- lib/postgres/models/shop.go
+++ lib/postgres/models/shop.go
@@ -1,7 +1,7 @@
 package models
 
 type Shop struct {
-	ID          string `json:"id" gorm:"column:id;primaryKey"`
+	Id          string `json:"id" gorm:"column:id;primaryKey"`
 	Name        string `json:"name" gorm:"column:name;not null"`
 	Image       string `json:"image" gorm:"column:image"`
 	Description string `json:"description" gorm:"column:description"`
--- lib/postgres/models/shopItem.go
+++ lib/postgres/models/shopItem.go
@@ -1,7 +1,7 @@
 package models
 
 type ShopItem struct {
-	ID     string `json:"id" gorm:"column:id;primaryKey"`
+	Id     string `json:"id" gorm:"column:id;primaryKey"`
 	ShopID string `json:"shop_id" gorm:"column:shop_id;not null"`
 	Name   string `json:"name" gorm:"column:name;not null"`
 	Image  string `json:"image" gorm:"column:image"`
