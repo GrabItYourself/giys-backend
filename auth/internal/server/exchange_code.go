23ed97eb3984b7144503a94167f41410a939d9a5
Nut Pinyo <bomb.np@gmail.com>
2022-10-05 23:17:29 +0700

    finish exchange code

--- auth/internal/server/exchange_code.go
+++ auth/internal/server/exchange_code.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 
 	"github.com/GrabItYourself/giys-backend/auth/pkg/authproto"
+	"github.com/GrabItYourself/giys-backend/lib/logger"
 	"github.com/GrabItYourself/giys-backend/lib/postgres/models"
 	"github.com/GrabItYourself/giys-backend/user/pkg/userproto"
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -33,15 +35,27 @@ func (s *Server) ExchangeAuthCode(ctx context.Context, in *authproto.ExchangeAut
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	resp, err := s.userClient.CreateUser(ctx, &userproto.CreateUserReq{
-		Email:    userInfo.Email,
-		GoogleId: userInfo.Id,
-	})
+	var user *userproto.User
+	getUserResp, err := s.userClient.GetUserByGoogleId(ctx, &userproto.GetUserByGoogleIdReq{GoogleId: userInfo.Id})
 	if err != nil {
-		return nil, status.Error(codes.Internal, errors.Wrap(err, "error during creating user").Error())
+		if status.Code(err) != codes.NotFound {
+			return nil, status.Error(codes.Internal, errors.Wrap(err, "can't get user by google id").Error())
+		}
+		logger.Info("user not found, creating new one", zap.String("google_id", userInfo.Id), zap.String("email", userInfo.Email))
+		createUserResp, err := s.userClient.CreateUser(ctx, &userproto.CreateUserReq{
+			Email:    userInfo.Email,
+			GoogleId: userInfo.Id,
+		})
+		user = createUserResp.User
+		if err != nil {
+			return nil, status.Error(codes.Internal, errors.Wrap(err, "error during creating user").Error())
+		}
+	} else {
+		logger.Info("user found", zap.String("google_id", userInfo.Id), zap.String("email", userInfo.Email))
+		user = getUserResp.User
 	}
 
-	accessToken, refreshToken, err := s.issueNewTokenPair(ctx, resp.User.Id, models.RoleEnum(resp.User.Role))
+	accessToken, refreshToken, err := s.issueNewTokenPair(ctx, user.Id, models.RoleEnum(user.Role))
 	if err != nil {
 		return nil, status.Error(codes.Internal, errors.Wrap(err, "failed to issue new token pair").Error())
 	}
