15f1626e522a10f479885d1d3e4cb485b73eca73
Nattamon Polvichai <joppy.pgh@gmail.com>
2022-11-11 00:19:49 +0700

    fix WHERE clause missing error

--- lib/postgres/models/shop_owner.go
+++ lib/postgres/models/shop_owner.go
@@ -1,9 +1,9 @@
 package models
 
 type ShopOwner struct {
-	ShopId string `json:"shop_id" gorm:"column:shop_id;not null"`
+	ShopId string `json:"shop_id" gorm:"column:shop_id;primaryKey;not null"`
 	Shop   Shop   `json:"shop" gorm:"foreignKey:ShopId;references:Id"`
-	UserId string `json:"user_id" gorm:"column:user_id;not null"`
+	UserId string `json:"user_id" gorm:"column:user_id;primaryKey;not null"`
 	User   User   `json:"user" gorm:"foreignKey:UserId;references:Id"`
 }
 
