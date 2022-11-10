78650ddf8af4e4c591b23923190ae6103009dcbe
nonya <nonya579@gmail.com>
2022-11-10 11:56:40 +0700

    fix migration

--- migrations/000013_add_default_payment_method_to_user_table.up.sql
+++ migrations/000013_add_default_payment_method_to_user_table.up.sql
@@ -6,7 +6,6 @@ ADD COLUMN "default_payment_method_id" INT;
 ALTER TABLE "user" 
 ADD CONSTRAINT fk_default_payment_method
 FOREIGN KEY (default_payment_method_id) 
-REFERENCES "payment_method" (id)
-ON DELETE CASCADE;
+REFERENCES "payment_method" (id);
 
 COMMIT;
