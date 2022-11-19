43c4dfbca3c8cd3e3b271f230a590e77337ef153
Nut Pinyo <bomb.np@gmail.com>
2022-11-19 17:55:48 +0700

    remove postgres from auth

--- auth/internal/repository/repository.go
+++ auth/internal/repository/repository.go
@@ -2,17 +2,14 @@ package repository
 
 import (
 	"github.com/go-redis/redis/v9"
-	"gorm.io/gorm"
 )
 
 type Repository struct {
-	pg  *gorm.DB
 	rdb *redis.Client
 }
 
-func New(pg *gorm.DB, rdb *redis.Client) *Repository {
+func New(rdb *redis.Client) *Repository {
 	return &Repository{
-		pg:  pg,
 		rdb: rdb,
 	}
 }
